1db1bc723ba011e65fecac4be3dce60bc2b3e022
Nishant Kumar <nishantkumarkd@gmail.com>
2022-06-19 23:15:17 +0530

    Starting the player at position 0

--- src/com/snakeandladder/SnakeAndLadder.java
+++ src/com/snakeandladder/SnakeAndLadder.java
@@ -2,6 +2,7 @@ package com.snakeandladder;
 
 public class SnakeAndLadder {
     public static void main(String[] args) {
-        System.out.println("Welcome to the game of Snake and Ladder");
+        int Player_Position = 0;
+        System.out.println("The player is initially at: "+Player_Position);
     }
 }
