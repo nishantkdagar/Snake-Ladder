cdf7ab6973026ebce5037409b5c65043754d100d
Nishant Kumar <nishantkumarkd@gmail.com>
2022-06-19 23:27:59 +0530

    Adding Snake and Ladder to the moves

--- src/com/snakeandladder/SnakeAndLadder.java
+++ src/com/snakeandladder/SnakeAndLadder.java
@@ -1,16 +1,24 @@
 package com.snakeandladder;
 import java.util.Random;
 public class SnakeAndLadder {
-    public static void main(String[] args) {
+    public static final int ladder = 0;
+    public static final int snake = 1;
+    public static void main(String[] args){
         int Player_Position = 0;
         Random rm = new Random();
         int Dice_Throw = rm.nextInt(7);
-        if (Dice_Throw > 0){
-            Player_Position = Player_Position + Dice_Throw;
-            System.out.println("The Player's new position is: "+Player_Position);
-        }
-        else {
-            System.out.println("The player is still at: "+Player_Position);
+        int Move = rm.nextInt(2);
+        switch (Move){
+            case ladder:
+                Player_Position = Player_Position + Dice_Throw;
+                System.out.println("The Player took the ladder and is at : "+Player_Position);
+                break;
+            case snake:
+                Player_Position = Player_Position - Dice_Throw;
+                System.out.println("The Player was bit by the snake and is at : "+Player_Position);
+                break;
+            default:
+                System.out.println("The Player didn't move at all and is at : "+Player_Position);
         }
     }
 }
