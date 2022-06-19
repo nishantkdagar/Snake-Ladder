6ef0cfccca91e82ee9d0e3133a8d4995b7e9453c
Nishant Kumar <nishantkumarkd@gmail.com>
2022-06-19 23:23:27 +0530

    Throwing the Dice

--- src/com/snakeandladder/SnakeAndLadder.java
+++ src/com/snakeandladder/SnakeAndLadder.java
@@ -1,8 +1,16 @@
 package com.snakeandladder;
-
+import java.util.Random;
 public class SnakeAndLadder {
     public static void main(String[] args) {
         int Player_Position = 0;
-        System.out.println("The player is initially at: "+Player_Position);
+        Random rm = new Random();
+        int Dice_Throw = rm.nextInt(7);
+        if (Dice_Throw > 0){
+            Player_Position = Player_Position + Dice_Throw;
+            System.out.println("The Player's new position is: "+Player_Position);
+        }
+        else {
+            System.out.println("The player is still at: "+Player_Position);
+        }
     }
 }
