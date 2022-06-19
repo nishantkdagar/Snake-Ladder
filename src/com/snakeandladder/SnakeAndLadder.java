670e93bbe071bb67f7aa385d2b21470665a6668c
Nishant Kumar <nishantkumarkd@gmail.com>
2022-06-19 23:11:39 +0530

    Programing Snake and Ladder

--- src/com/snakeandladder/SnakeAndLadder.java
+++ src/com/snakeandladder/SnakeAndLadder.java
@@ -2,6 +2,6 @@ package com.snakeandladder;
 
 public class SnakeAndLadder {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Game of Snake and Ladder");
+        System.out.println("Welcome to the game of Snake and Ladder");
     }
 }
