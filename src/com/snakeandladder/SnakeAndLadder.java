b267443ceecc1be02ab300eb8bc6d50987031577
Nishant Kumar <nishantkumarkd@gmail.com>
2022-06-20 21:09:12 +0530

    Added code to play the game till 100

--- src/com/snakeandladder/SnakeAndLadder.java
+++ src/com/snakeandladder/SnakeAndLadder.java
@@ -6,19 +6,30 @@ public class SnakeAndLadder {
     public static void main(String[] args){
         int Player_Position = 0;
         Random rm = new Random();
-        int Dice_Throw = rm.nextInt(7);
-        int Move = rm.nextInt(3);
-        switch (Move){
-            case ladder:
-                Player_Position = Player_Position + Dice_Throw;
-                System.out.println("The Player took the ladder and is at : "+Player_Position);
-                break;
-            case snake:
-                Player_Position = Player_Position - Dice_Throw;
-                System.out.println("The Player was bit by the snake and is at : "+Player_Position);
-                break;
-            default:
-                System.out.println("The Player didn't move at all and is at : "+Player_Position);
+
+        while (Player_Position <= 100) {
+            int Dice_Throw = rm.nextInt(6) + 1;
+            int Move = rm.nextInt(3);
+
+            switch (Move) {
+                case ladder:
+                    if(Player_Position + Dice_Throw <= 100){
+                        Player_Position = Player_Position + Dice_Throw;
+                    }
+                    System.out.println("The Player took the ladder and is at : " + Player_Position);
+                    break;
+                case snake:
+                    if (Player_Position - Dice_Throw > 0) {
+                        Player_Position = Player_Position - Dice_Throw;
+                    }
+                    else {
+                        Player_Position = 0;
+                    }
+                    System.out.println("The Player was bit by the snake and is at : " + Player_Position);
+                    break;
+                default:
+                    System.out.println("The Player didn't move at all and is at : " + Player_Position);
+            }
         }
     }
 }
